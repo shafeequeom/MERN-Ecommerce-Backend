ff4b0d4ccb90da7b364c6c2e604bc1b73b128724
Shafeeque OM <5572374-shafeequekrews@users.noreply.gitlab.com>
2022-01-22 09:10:01 +0400

    Admin Cheker API

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 //Middlewares
-const { authCheck } = require("../middlewares/auth");
+const { authCheck, adminCheck } = require("../middlewares/auth");
 
 //Controllers
 const { createOrUpdateUser, currentUser } = require("../controllers/auth");
@@ -11,5 +11,6 @@ const { createOrUpdateUser, currentUser } = require("../controllers/auth");
 //route
 router.post("/user", authCheck, createOrUpdateUser);
 router.post("/user/current", authCheck, currentUser);
+router.post("/user/admin", authCheck, adminCheck, currentUser);
 
 module.exports = router;
