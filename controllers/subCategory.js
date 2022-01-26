cd38bc28915ac306870159e853fa70fcee9a9568
Shafeeque OM <5572374-shafeequekrews@users.noreply.gitlab.com>
2022-01-26 19:08:44 +0400

    Sub Category CRUD APIs

---
+++ controllers/subCategory.js
@@ -0,0 +1,46 @@
+const SubCategory = require("../models/subCategory");
+const slugify = require("slugify");
+
+exports.create = async (req, res) => {
+  try {
+    const { name, parent } = req.body;
+    const subCategory = await new SubCategory({
+      name,
+      parent,
+      slug: slugify(name),
+    }).save();
+    res.json(subCategory);
+  } catch (error) {
+    res.status(400).send("Create sub-category failed");
+  }
+};
+exports.read = async (req, res) => {
+  const data = await SubCategory.findOne({ slug: req.params.slug });
+  res.json(data);
+};
+exports.remove = async (req, res) => {
+  try {
+    const data = await SubCategory.findOneAndDelete({ slug: req.params.slug });
+    res.json(data);
+  } catch (error) {
+    res.status(400).send("Delete sub-category failed");
+  }
+};
+exports.list = async (req, res) => {
+  const data = await SubCategory.find({}).sort({ createdAt: -1 }).exec();
+  res.json(data);
+};
+
+exports.update = async (req, res) => {
+  const { name, parent } = req.body;
+  try {
+    const data = await SubCategory.findOneAndUpdate(
+      { slug: req.params.slug },
+      { name, parent, slug: slugify(name) },
+      { new: true }
+    );
+    res.json(data);
+  } catch (error) {
+    res.status(400).send("Update sub-category failed");
+  }
+};
---
+++ routes/subCategory.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const router = express.Router();
+
+//Middlewares
+const { authCheck, adminCheck } = require("../middlewares/auth");
+
+//Controllers
+const {
+  create,
+  read,
+  update,
+  remove,
+  list,
+} = require("../controllers/subCategory");
+
+//route
+router.post("/sub-category", authCheck, adminCheck, create);
+router.get("/sub-category/:slug", read);
+router.get("/sub-categories", list);
+router.put("/sub-category/:slug", authCheck, adminCheck, update);
+router.delete("/sub-category/:slug", authCheck, adminCheck, remove);
+
+module.exports = router;
